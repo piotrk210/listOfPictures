df54b17441b3856cdee396c0496558f3d888f4d0
Piotr Jakubowski <piotrk210@wp.pl>
2022-05-24 21:03:50 +0200

    added TextMeshPro, new cell prefab, renamed folders

---
+++ list/Assets/_Scripts/Cell.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cell : MonoBehaviour
+{
+    public TextMeshProUGUI fileNameText;
+    public TextMeshProUGUI creationTimeText;
+    public RawImage RawImage;
+
+    public void UpdateImage()
+    {
+        
+    }
+
+    public void UpdateText()
+    {
+        
+    }
+
+}
